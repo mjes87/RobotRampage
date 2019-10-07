d88f142d60c81342ed5af7f3fb354eb36e67cfa9
Mike Sullivan <mjes87@gmail.com>
2019-10-07 16:12:34 -0400

    Added reticles to weapons

--- Assets/Scripts/GunEquipper.cs
+++ Assets/Scripts/GunEquipper.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GunEquipper : MonoBehaviour
 {
+    [SerializeField]
+    GameUI gameUI;
+
     public static string activeWeaponType;
     public GameObject pistol;
     public GameObject assaultRifle;
@@ -40,16 +43,19 @@ public class GunEquipper : MonoBehaviour
         {
             loadWeapon(pistol);
             activeWeaponType = Constants.Pistol;
+            gameUI.UpdateReticle();
         }
-        if (Input.GetKeyDown("2"))
+        else if (Input.GetKeyDown("2"))
         {
             loadWeapon(assaultRifle);
             activeWeaponType = Constants.AssaultRifle;
+            gameUI.UpdateReticle();
         }
         else if (Input.GetKeyDown("3"))
         {
             loadWeapon(shotgun);
             activeWeaponType = Constants.Shotgun;
+            gameUI.UpdateReticle();
         }
     }
 }
