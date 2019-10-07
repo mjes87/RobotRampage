9383d2c3fe6037462cb8e6d4ae0710572fefea93
Mike Sullivan <mjes87@gmail.com>
2019-10-07 15:45:14 -0400

    Added Firing Scripts

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Constants
+﻿public class Constants
 {
     // Scenes
     public const string SceneBattle = "Battle";
