722518602b112ad41653b8bea43cfcc256d6884a
Mike Sullivan <mjes87@gmail.com>
2019-10-07 15:39:50 -0400

    Added Constants and GunEquipper
    Constants declared in script and weapons an now be selected via numbers 1-3 on keyboard

---
+++ Assets/Scripts/GunEquipper.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunEquipper : MonoBehaviour
+{
+    public static string activeWeaponType;
+    public GameObject pistol;
+    public GameObject assaultRifle;
+    public GameObject shotgun;
+    GameObject activeGun;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeWeaponType = Constants.Pistol;
+        activeGun = pistol;
+    }
+
+    // Loads the specified chosen weapon
+    private void loadWeapon(GameObject weapon)
+    {
+        pistol.SetActive(false);
+        assaultRifle.SetActive(false);
+        shotgun.SetActive(false);
+
+        weapon.SetActive(true);
+        activeGun = weapon;
+    }
+
+    public GameObject GetActiveWeapon()
+    {
+        return activeGun;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            loadWeapon(pistol);
+            activeWeaponType = Constants.Pistol;
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            loadWeapon(assaultRifle);
+            activeWeaponType = Constants.AssaultRifle;
+        }
+        else if (Input.GetKeyDown("3"))
+        {
+            loadWeapon(shotgun);
+            activeWeaponType = Constants.Shotgun;
+        }
+    }
+}
