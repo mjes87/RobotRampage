ae7b3d10d5099a1c0729a1bd9f0375b7d048fba3
Mike Sullivan <mjes87@gmail.com>
2019-11-01 12:52:48 -0400

    Chapter 9 Complete

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     public float fireRate;
+    public Ammo ammo;
+    public AudioClip liveFire;
+    public AudioClip dryFire;
+
     protected float lastFireTime;
 
     // Start is called before the first frame update
@@ -20,6 +24,15 @@ public class Gun : MonoBehaviour
     }
     protected void Fire()
     {
+        if (ammo.HasAmmo(tag))
+        {
+            GetComponent<AudioSource>().PlayOneShot(liveFire);
+            ammo.ConsumeAmmo(tag);
+        }
+        else
+        {
+            GetComponent<AudioSource>().PlayOneShot(dryFire);
+        }
         GetComponentInChildren<Animator>().Play("Fire");
     }
 }
