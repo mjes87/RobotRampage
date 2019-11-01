c837bfd32f5ed465abe2f978539ec24bb25648bb
Mike Sullivan <mjes87@gmail.com>
2019-11-01 13:24:43 -0400

    Added Missile Projectiles to Robots

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Robot : MonoBehaviour
 {
     [SerializeField]
+    GameObject missileprefab;
+
+    [SerializeField]
     private string robotType;
 
     public int health;
@@ -53,6 +56,9 @@ public class Robot : MonoBehaviour
 
     private void fire()
     {
+        GameObject missile = Instantiate(missileprefab);
+        missile.transform.position = missileFireSpot.transform.position;
+        missile.transform.rotation = missileFireSpot.transform.rotation;
         robot.Play("Fire");
     }
 }
