92ef4b0daeb983ba318c9b42e8a12aa65c6d943e
Mike Sullivan <mjes87@gmail.com>
2019-11-08 14:32:57 -0500

    Added Main Menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 1
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Battle");
+    }
+    // 2
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
